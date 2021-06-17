2efb2f1d5d36a5b81a8fd40c52cc70ef7d2f0590
Szymon Bednorz <dsonyyy@gmail.com>
2021-06-18 01:32:54 +0200

    Cloud rotating system.

--- src/cloud.js
+++ src/cloud.js
@@ -36,10 +36,12 @@ export default class Cloud {
     // Nodes
     this.nodes = Cloud.createNodes(20);
     this.scene.add(this.nodes);
+
+    this.mouseRotation = new MouseRotation(this.element, this.nodes);
   }
 
   update() {
-    console.log(this.element);
+    this.mouseRotation.update();
   }
 
   render() {
@@ -81,5 +83,81 @@ export default class Cloud {
     }
     group.rotation.z = Math.PI / 2;
     return group;
+  }
+}
+
+class MouseRotation {
+  constructor(element, object) {
+    this.element = element;
+    this.object = object;
+
+    this.pointerHolding = false;
+    this.slowingDown = false;
+
+    this.slowDownFactor = 0.97;
+    this.moveSpeedFactor = 0.0001;
+    this.moveSpeedMax = 400;
+    this.moveSpeedMin = 3;
+
+    this.vectorX = 0;
+    this.vectorY = 0.7;
+
+    document.addEventListener("pointermove", (e) => this.onPointerMove(e));
+    document.addEventListener("pointerdown", (e) => this.onPointerDown(e));
+    document.addEventListener("pointerup", (e) => this.onPointerUp(e));
+  }
+
+  update() {
+    if (Math.abs(this.vectorY) > this.moveSpeedMax)
+      this.vectorY = Math.sign(this.vectorY) * this.moveSpeedMax;
+    if (Math.abs(this.vectorX) > this.moveSpeedMax)
+      this.vectorX = Math.sign(this.vectorX) * this.moveSpeedMax;
+
+    if (this.pointerHolding) {
+      this.object.rotateOnWorldAxis(
+        new THREE.Vector3(0, 1, 0),
+        this.vectorY * this.moveSpeedFactor
+      );
+      this.object.rotateOnWorldAxis(
+        new THREE.Vector3(1, 0, 0),
+        this.vectorX * this.moveSpeedFactor
+      );
+    } else {
+      this.object.rotateOnWorldAxis(
+        new THREE.Vector3(0, 1, 0),
+        this.vectorY * this.moveSpeedFactor
+      );
+      this.object.rotateOnWorldAxis(
+        new THREE.Vector3(1, 0, 0),
+        this.vectorX * this.moveSpeedFactor
+      );
+
+      // Slowing down
+      if (this.slowingDown) {
+        this.vectorY *= this.slowDownFactor;
+        if (Math.abs(this.vectorY) < this.moveSpeedMin) this.vectorY = 0;
+
+        this.vectorX *= this.slowDownFactor;
+        if (Math.abs(this.vectorX) < this.moveSpeedMin) this.vectorX = 0;
+      }
+    }
+  }
+
+  onPointerMove(e) {
+    if (this.pointerHolding) {
+      this.vectorY += e.movementX;
+      this.vectorX += e.movementY;
+    }
+  }
+
+  onPointerDown(e) {
+    this.vectorY = 0;
+    this.vectorX = 0;
+    this.pointerHolding = true;
+  }
+
+  onPointerUp(e) {
+    this.pointerHolding = false;
+    this.slowingDown = true;
   }
 }
